a2e62a627092690a95c2661c6da492a6c93b2369
Anh Phung <50242713+pvaconnor@users.noreply.github.com>
2020-10-08 21:29:50 -0700

    Creating splash screen

---
+++ assignment5/app/src/main/java/com/example/assignment5/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.example.assignment5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        TimerTask task = new TimerTask(){
+            public void run() {
+                finish();
+                startActivity(new Intent( SplashActivity.this, MainActivity.class) );
+            }
+        };
+        Timer opening = new Timer();
+        opening.schedule(task,5000);
+    }
+}
