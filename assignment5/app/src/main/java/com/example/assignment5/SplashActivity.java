e2ddc0b49dcd6eec13b74f1f33c71a2f0087d459
Anh Phung <50242713+pvaconnor@users.noreply.github.com>
2020-10-08 21:57:12 -0700

    Adding audio in splash

--- assignment5/app/src/main/java/com/example/assignment5/SplashActivity.java
+++ assignment5/app/src/main/java/com/example/assignment5/SplashActivity.java
@@ -3,6 +3,7 @@ package com.example.assignment5;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import java.util.Timer;
@@ -10,12 +11,17 @@ import java.util.TimerTask;
 
 public class SplashActivity extends AppCompatActivity {
 
+    MediaPlayer frozen;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        frozen = MediaPlayer.create(this, R.raw.frozen);
+        frozen.start();
         TimerTask task = new TimerTask(){
             public void run() {
+                frozen.stop();
                 finish();
                 startActivity(new Intent( SplashActivity.this, MainActivity.class) );
             }
