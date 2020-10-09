b52bce603492177f854b66e289f08f64949b24da
Anh Phung <50242713+pvaconnor@users.noreply.github.com>
2020-10-08 20:46:49 -0700

    finishing assignment 4

--- assignment5/app/src/main/java/com/example/assignment5/MainActivity.java
+++ assignment5/app/src/main/java/com/example/assignment5/MainActivity.java
@@ -16,10 +16,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String[] mTitleList = new String[]{getResources().getString(R.string.dish1), getResources().getString(R.string.dish2),
-                getResources().getString(R.string.dish3), getResources().getString(R.string.dish4), getResources().getString(R.string.dish5), getResources().getString(R.string.dish6)};
-        String[] mDescription = new String[]{getResources().getString(R.string.dish11), getResources().getString(R.string.dish22),
-                getResources().getString(R.string.dish33), getResources().getString(R.string.dish44), getResources().getString(R.string.dish55), getResources().getString(R.string.dish66)};
+        String[] mTitleList = new String[]{getResources().getString(R.string.d1),
+                getResources().getString(R.string.d2),
+                getResources().getString(R.string.d3),
+                getResources().getString(R.string.d4),
+                getResources().getString(R.string.d5),
+                getResources().getString(R.string.d6)};
+        String[] mDescription = new String[]{getResources().getString(R.string.d1),
+                getResources().getString(R.string.d2),
+                getResources().getString(R.string.d3),
+                getResources().getString(R.string.d4),
+                getResources().getString(R.string.d5),
+                getResources().getString(R.string.d6)};
         mRecyclerView = findViewById(R.id.recyclerview);
         mAdapter = new WordListAdapter(this, mTitleList, mDescription);
         mRecyclerView.setAdapter(mAdapter);
